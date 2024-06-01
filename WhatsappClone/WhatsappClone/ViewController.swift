f901cfccc521c2832bfda88ed9e33a09d58fdebf
Nanda <nanda.perdana@kuncie.com>
2024-06-01 22:19:36 +0800

    create new project

---
+++ WhatsappClone/WhatsappClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsappClone
+//
+//  Created by nandawperdana on 01/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

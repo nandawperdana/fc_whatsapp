0f9ff2875311bdc8f5dde6f37ea689abfcc3ba41
Nanda <nanda.perdana@kuncie.com>
2024-06-03 23:00:40 +0800

    implement UI login and register

---
+++ WhatsappClone/WhatsappClone/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  WhatsappClone
+//
+//  Created by nandawperdana on 03/06/24.
+//
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+

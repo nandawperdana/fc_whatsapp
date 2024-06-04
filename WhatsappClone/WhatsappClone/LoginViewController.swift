48ad609812b3c44aa74d92cf9770ce77ba98e0e4
Nanda <nanda.perdana@kuncie.com>
2024-06-04 22:53:09 +0800

    IBOutlets connection

--- WhatsappClone/WhatsappClone/LoginViewController.swift
+++ WhatsappClone/WhatsappClone/LoginViewController.swift
@@ -7,10 +7,37 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    // Label
+    @IBOutlet weak var registerLabel: UILabel!
+    
+    // Text Field
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    // Button
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - IBActions
+    @IBAction func loginButtonTap(_ sender: Any) {
+        print("loginButtonTap")
+    }
+    
+    @IBAction func forgotPasswordButtonTap(_ sender: Any) {
+        print("forgotPasswordButtonTap")
+    }
+    
+    @IBAction func registerButtonTap(_ sender: Any) {
+        print("registerButtonTap")
+    }
+    
 }
 

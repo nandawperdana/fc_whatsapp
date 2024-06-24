9e4fb2c906ca86759ea8957d73fa8b75992ca3f2
Nanda <nanda.perdana@kuncie.com>
2024-06-24 08:40:01 +0700

    create contacts list

---
+++ WhatsappClone/WhatsappClone/View/ContactsView/ContactsTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  ContactsTableViewCell.swift
+//  WhatsappClone
+//
+//  Created by nandawperdana on 24/06/24.
+//
+
+import UIKit
+
+class ContactsTableViewCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    func configure(user: User) {
+        usernameLabel.text = user.username
+        statusLabel.text = ""
+        setAvatar(avatar: user.avatar)
+    }
+    
+    private func setAvatar(avatar: String) {
+        guard avatar != "" else { return }
+        
+        FirebaseStorageHelper.downloadImage(url: avatar) { image in
+            self.avatarImageView.image = image
+        }
+    }
+}

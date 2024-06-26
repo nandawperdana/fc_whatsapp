e956bd18723d89b15ae713a7b16d9bf574294daf
Nanda <nanda.perdana@kuncie.com>
2024-06-26 10:00:02 +0700

    build recent chat table view cell

---
+++ WhatsappClone/WhatsappClone/View/RecentChatsView/RecentChatTableViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  RecentChatTableViewCell.swift
+//  WhatsappClone
+//
+//  Created by nandawperdana on 26/06/24.
+//
+
+import UIKit
+
+class RecentChatTableViewCell: UITableViewCell {
+    //MARK: IBOutlets
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var recentMessageLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var unreadCounterLabel: UILabel!
+    @IBOutlet weak var unreadCounterView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.width / 2
+        unreadCounterView.layer.cornerRadius = unreadCounterView.frame.width / 2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    // MARK: - Configure Cell
+    func configure(recent: RecentChat) {
+        usernameLabel.text = recent.receiverName
+        usernameLabel.adjustsFontSizeToFitWidth = true
+        usernameLabel.minimumScaleFactor = 0.5
+        
+        recentMessageLabel.text = recent.lastMessage
+        recentMessageLabel.adjustsFontSizeToFitWidth = true
+        recentMessageLabel.numberOfLines = 2
+        recentMessageLabel.minimumScaleFactor = 0.9
+        
+        if recent.unreadCounter != 0 {
+            self.unreadCounterLabel.text = "\(recent.unreadCounter)"
+            self.unreadCounterView.isHidden = false
+        } else {
+            self.unreadCounterView.isHidden = true
+        }
+        
+        setAvatar(avatar: recent.avatar)
+        dateLabel.text = formatDate(date: recent.date!)
+        dateLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    private func setAvatar(avatar: String) {
+        guard avatar != "" else { return }
+        
+        FirebaseStorageHelper.downloadImage(url: avatar) { image in
+            self.avatarImageView.image = image
+        }
+    }
+    
+    private func formatDate(date: Date) -> String {
+        return ""
+    }
+}
